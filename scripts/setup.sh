97fe5794a751815c5be7291758aa38a0a4594d69
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-13 17:45:08 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
 # Ensure the app folder exists
-mkdir -p /home/ubuntu/app
+mkdir -p /home/ubuntu/ignite-java-asg
  
 # Ensure the script has execution permissions
-chmod +x /home/ubuntu/scripts/run_jar.sh
+chmod +x /home/ubuntu/ignite-java-asg/scripts/run_jar.sh
  
 # Optionally, clean up any old application data if necessary
 # rm -rf /home/ubuntu/app/*
