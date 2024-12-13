32bf30b8efc1982910785feb504287e9b453697d
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-13 16:33:01 +0530

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+# Ensure the app folder exists
+mkdir -p /home/ubuntu/app
+ 
+# Ensure the script has execution permissions
+chmod +x /home/ubuntu/scripts/run_jar.sh
+ 
+# Optionally, clean up any old application data if necessary
+# rm -rf /home/ubuntu/app/*
