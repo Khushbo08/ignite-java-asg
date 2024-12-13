c4ae4c2c1337654e2bf4e215b22644ff5692c242
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-13 17:51:03 +0530

    Update run_jar.sh

--- scripts/run_jar.sh
+++ scripts/run_jar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  
 # Path to the JAR file
-JAR_PATH="/home/ubuntu/app/app.jar"
+JAR_PATH="/home/ubuntu/ignite-java-asg/Employee-Management-System-0.0.1-SNAPSHOT.jar"
  
 # Check if the JAR file exists
 if [ -f "$JAR_PATH" ]; then
   echo "Starting the application..."
   # Run the JAR file in the background
-  nohup java -jar $JAR_PATH > /home/ubuntu/app/app.log 2>&1 &
+  nohup java -jar $JAR_PATH > /home/ubuntu/ignite-java-asg/Employee-Management-System-0.0.1-SNAPSHOT.log 2>&1 &
 else
   echo "JAR file not found!"
   exit 1
