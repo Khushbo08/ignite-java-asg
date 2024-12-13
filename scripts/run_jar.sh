bc8cf564675ec6c0febd78f0b1d80dbc9810fd38
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-13 16:38:16 +0530

    Create run_jar.sh

---
+++ scripts/run_jar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ 
+# Path to the JAR file
+JAR_PATH="/home/ubuntu/app/app.jar"
+ 
+# Check if the JAR file exists
+if [ -f "$JAR_PATH" ]; then
+  echo "Starting the application..."
+  # Run the JAR file in the background
+  nohup java -jar $JAR_PATH > /home/ubuntu/app/app.log 2>&1 &
+else
+  echo "JAR file not found!"
+  exit 1
+fi
