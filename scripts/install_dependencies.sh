0307dd551e9183cfe3e87a149181c9008018c944
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-15 02:29:42 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,10 +3,6 @@ echo "Installing dependencies..."
 # Update package lists
 sudo apt update -y
 # Install OpenJDK 17 and Maven
-sudo apt install -y openjdk-17-jdk maven curl
-# Check if installation was successful
-if [[ $? -ne 0 ]]; then
-    echo "Error: Failed to install dependencies." >&2
-    exit 1
-fi 
-echo "Dependencies installed successfully!"
+sudo apt install -y openjdk-17-jdk 
+sudo apt install maven -y
+
