9378632342ba720aed07758a413beaa68e38e4b6
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-15 15:08:58 +0530

    Update configure_application.sh

--- scripts/configure_application.sh
+++ scripts/configure_application.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Configuring application..."
-sudo mkdir /home/ec2-user
-sudo mkdir -p /home/ec2-user/ignite-java-asg/config
-chown ec2-user:ec2-user /home/ec2-usere/ignite-java-asg
+sudo mkdir -p /home/ubuntu/ignite-java-asg/config
+chown ubuntu:ubuntu /home/ubuntu/ignite-java-asg
