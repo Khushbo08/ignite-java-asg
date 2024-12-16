9c43da31ca151e594b171c137873781a029c3f3e
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-16 17:58:26 +0530

    Update configure_app.sh

--- scripts/configure_app.sh
+++ scripts/configure_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Configuring application..."
-sudo mkdir -p /home/ec2-user/employee-management-system/config
-chown ubuntu:ubuntu /home/ec2-user/employee-management-system
+sudo mkdir -p /home/ubuntu/employee-management-system/config
+chown ubuntu:ubuntu /home/ubuntu/employee-management-system
