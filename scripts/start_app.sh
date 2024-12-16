0706fc936c2c7eff635e0f57b59ba7d427871c0a
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-16 18:25:48 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting application..."
-nohup java -jar /home/ubuntu/employee-management-system/Employee-Management-System-0.0.1-SNAPSHOT.jar > /home/ubuntu/employee-management-system/app.log 2>&1 &
+nohup java -jar /home/ubuntu/ignite-java-asg/*.jar > /home/ubuntu/ignite-java-asg/app.log 2>&1 &
 
