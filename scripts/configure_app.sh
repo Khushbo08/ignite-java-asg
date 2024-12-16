0872f7c38072434754da78c33bd3b8dde43a7f77
Khushbu Burde <114572964+Khushbo08@users.noreply.github.com>
2024-12-16 17:24:21 +0530

    Update and rename configure_application.sh to configure_app.sh

---
+++ scripts/configure_app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Configuring application..."
+sudo mkdir -p /home/ec2-user/employee-management-system/config
+chown ubuntu:ubuntu /home/ec2-user/employee-management-system
--- scripts/configure_application.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Configuring application..."
-sudo mkdir -p /home/ubuntu/ignite-java-asg/config
-chown ubuntu:ubuntu /home/ubuntu/ignite-java-asg
